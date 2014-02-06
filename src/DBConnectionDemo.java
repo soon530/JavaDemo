5cd2c3353c607fe4d64a6c45484cbe874134442d
soon <soon530@gmail.com>
2014-02-07 01:07:27 +0800

    ASSIGNED - bug 66368: [Managed Build] Builder configurations should be parametrized https://bugs.eclipse.org/bugs/show_bug.cgi?id=66368
    Signed-off-by: soon <soon530@gmail.com>

--- src/DBConnectionDemo.java
+++ src/DBConnectionDemo.java
@@ -14,12 +14,12 @@ public class DBConnectionDemo {
 		// 每一個driver字串都不太一樣
 		String driver = "com.mysql.jdbc.Driver";
 		// 因為要用中文，所以就把所有的資訊都用url來表示，不將user & password分開
-		String url = "jdbc:mysql://localhost:3306/matchman?user=root&password=such0720&useUnicode=true&characterEncoding=UTF-8";
+		String url = "jdbc:mysql://192.168.0.33:3306/tasker?user=root&password=such0720&useUnicode=true&characterEncoding=UTF-8";
 		try {
-			/* 
+			/*
 			 * 透過java.lang.Class類別的forName()來載入並向DriverManager註冊JDBC驅動程式
 			 * （驅動程式會自動透過DriverManager.registerDriver()方法註冊）
-			*/
+			 */
 			Class.forName(driver);
 			Connection conn = DriverManager.getConnection(url);
 
